9a87d77cc4640a768359bb5d03e6768c07fb9a00
javadEsl <javades755@gmail.com>
2024-04-19 16:25:11 +0430

    Fixed: information and resource files and fix some styles

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -17,11 +17,11 @@ const Contact = () => {
                     whileInView={{opacity:1,x:0}}
                     transition={{duration:1}}
                     className="my-4">{CONTACT.address}</motion.p>
-                <motion.p
-                    initial={{opacity:0 , x:100}}
-                    whileInView={{opacity:1,x:0}}
-                    transition={{duration:1}}
-                    className="my-4">{CONTACT.phoneNo}</motion.p>
+                {/*<motion.p*/}
+                {/*    initial={{opacity:0 , x:100}}*/}
+                {/*    whileInView={{opacity:1,x:0}}*/}
+                {/*    transition={{duration:1}}*/}
+                {/*    className="my-4">{CONTACT.phoneNo}</motion.p>*/}
                 <a href="#" className=" border-b">{CONTACT.email}</a>
             </div>
         </div>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -18,8 +18,8 @@ const Projects = () => {
                             initial={{opacity:0 , x:-100}}
                             whileInView={{opacity:1,x:0}}
                             transition={{duration:1}}
-                            className="w-full lg:w-1/4">
-                            <img  style={{width:"95%" , height:"170px" , objectFit:"cover"}} src={project.image.src} alt={project.title} width={150} height={150}
+                            className="w-full md:w-1/2 lg:w-1/4">
+                            <img  style={{width:"95%" , height:"150px" , objectFit:"cover"}} src={project.image.src} alt={project.title} width={150} height={150}
                                  className={'mb-6 rounded'}/>
                         </motion.div>
                         <motion.div
